515cb60b6cb6f33209fde040eeecbbfa19725680
Stanislav Benet <stanislav.benet@gmail.com>
2023-04-10 17:23:12 +0300

    Initial commit

---
+++ server/controllers/typeController.js
@@ -0,0 +1,9 @@
+const {Type} = require('../models')
+
+
+class TypeController {
+  async create(req, res) {}
+  async getAll(req, res) {}
+}
+
+module.exports = new TypeController();
---
+++ server/index.js
@@ -0,0 +1,27 @@
+require("dotenv").config();
+const express = require("express");
+const sequelize = require("./db");
+const models = require("./models/models");
+const cors = require("cors");
+const router = require("./routes");
+const errorHandler = require("./middleware/ErrorHandlingMiddleware");
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use("/api", router);
+
+app.use(errorHandler);
+
+const start = async () => {
+  try {
+    await sequelize.authenticate();
+    await sequelize.sync();
+    app.listen(PORT, () => console.log(`Server start on ${PORT}`));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
